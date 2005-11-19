b14224f8d7b25e8beaddbe7cdd56af1655bf5830
Don Stewart <dons@cse.unsw.edu.au>
2005-11-19 02:53:14 -0800

    Pretty print  ^L in help screen
    darcs-hash:8e167659c7020164610446a11e784907e74bf6bf

--- UI.hs
+++ UI.hs
@@ -283,6 +283,7 @@ instance Element HelpScreen where
                           | k == Curses.keyLeft  -> "Left"
                           | k == Curses.keyRight -> "Right"
                           | k == '\n'            -> "Enter"
+                          | k == '\f'            -> "^L"
                           | k == Curses.keyEnd   -> "End"
                           | k == Curses.keyHome  -> "Home"
                         _ -> show c
