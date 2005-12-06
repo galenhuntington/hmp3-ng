b2b5ff0b3a8686de65197946f523581bbe0c0088
Don Stewart <dons@cse.unsw.edu.au>
2005-12-05 17:40:15 -0800

    Should be "config.h"
    darcs-hash:fed46c89353cee65d2deb14ed3b26c2e385951fb

--- cbits/utils.h
+++ cbits/utils.h
@@ -2,9 +2,9 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <errno.h>
-
 #include <curses.h>
-#include <config.h>
+
+#include "config.h"
 
 /* curses */
 extern void nomacro_getyx(WINDOW *win, int *y, int *x);
