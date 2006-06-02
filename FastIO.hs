e7155841d9d7892adad81b30e5ba41aa6df470b0
Don Stewart <dons@cse.unsw.edu.au>
2006-06-01 21:15:49 -0700

    Port to latest bytestring  api
    darcs-hash:710802ef7df254c74b7772c1748f93e411a850b9

--- FastIO.hs
+++ FastIO.hs
@@ -140,7 +140,7 @@ isDirectory stat = do
 -- Returning a ByteString. Note that the underlying C code is dropping
 -- redundant \@F frames for us.
 getFilteredPacket :: Ptr CFile -> IO P.ByteString
-getFilteredPacket fp = B.generate size $ \p -> do 
+getFilteredPacket fp = B.createAndTrim size $ \p -> do 
     i <- c_getline p fp
     if i == -1 
         then throwErrno "FastIO.packedHGetLine"
@@ -185,7 +185,7 @@ send h m = P.hPut h (ppr m) >> P.hPut h nl >> hFlush h
 -- 
 printfPS :: P.ByteString -> Int -> Int -> P.ByteString
 printfPS fmt arg1 arg2 =
-    unsafePerformIO $ B.generate lim $ \ptr ->
+    unsafePerformIO $ B.createAndTrim lim $ \ptr ->
         B.unsafeUseAsCString fmt $ \c_fmt -> do
             sz' <- c_printf2d ptr (fromIntegral lim) (castPtr c_fmt)
                         (fromIntegral arg1) (fromIntegral arg2)
