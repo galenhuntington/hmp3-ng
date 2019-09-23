dadb241747217352f6f2dd6d9fc35b5ca22d3677
Galen Huntington <galen@alumni.reed.edu>
2019-09-22 20:51:59 -0700

    Authorship tweaks.

--- Keymap.hs
+++ Keymap.hs
@@ -1,6 +1,6 @@
 -- 
 -- Copyright (c) 2004-2008 Don Stewart - http://www.cse.unsw.edu.au/~dons
--- Copyright (c) 2008 Galen Huntington
+-- Copyright (c) 2008, 2019 Galen Huntington
 -- 
 -- This program is free software; you can redistribute it and/or
 -- modify it under the terms of the GNU General Public License as
