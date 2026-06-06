76336dab0bbbb6146a30147c382cc184d5296868
Galen Huntington <galen.huntington@gmail.com>
2026-06-06 11:53:18 -0700

    Drop unused Bifunctor import.

--- Base.hs
+++ Base.hs
@@ -13,7 +13,6 @@ import Prelude
 import Control.Concurrent as X
 import Control.Exception as X
 import Control.Monad as X
-import Data.Bifunctor as X
 import Data.ByteString as X (ByteString)
 import Data.Char as X
 import Data.Fixed as X
