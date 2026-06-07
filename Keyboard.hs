b3d29dee5bce88e49641c7b9cb414d3e1ee011e1
Galen Huntington <galen.huntington@gmail.com>
2026-06-07 11:16:44 -0700

    Module for keyboard, ending boot file.

---
+++ Keyboard.hs
@@ -0,0 +1,31 @@
+{-# OPTIONS -Wno-orphans #-}
+
+-- Copyright (c) 2019-2026 Galen Huntington
+-- SPDX-License-Identifier: GPL-2.0-or-later
+
+module Keyboard (unkey, charToKey, Key(..)) where
+
+import Base
+
+import qualified Data.Map.Strict as M
+import UI.HSCurses.Curses (Key(..), decodeKey)
+
+------------------------------------------------------------------------
+-- Char ↔ Key translation
+--
+-- ncurses delivers special keys as integer codes ≥ 256; for everything
+-- in 0..255 'decodeKey' returns 'KeyChar (chr n)'.  We keep working in
+-- 'Char' (UI.getKey's type), so we extend the range up to '\500' to
+-- cover the named keys we actually use (KEY_RESIZE is around 410).
+
+deriving stock instance Ord Key
+
+charToKey :: Char -> Key
+charToKey = decodeKey . toEnum . fromEnum
+
+keyCharMap :: M.Map Key Char
+keyCharMap = M.fromList [(charToKey c, c) | c <- ['\0' .. '\500']]
+
+unkey :: Key -> Char
+unkey k = fromMaybe '\0' $ M.lookup k keyCharMap
+
--- Keymap.hs-boot
+++
@@ -1,7 +0,0 @@
-module Keymap where
-
-import UI.HSCurses.Curses (Key)
-
-unkey      :: Key -> Char
-charToKey  :: Char -> Key
-
