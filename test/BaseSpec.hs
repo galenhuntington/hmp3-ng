2aaf1d221be0b5d64ba42fa921a3e79e7734bef3
Galen Huntington <galen.huntington@gmail.com>
2026-06-20 14:16:51 -0700

    Base test tweaks.

--- test/BaseSpec.hs
+++ test/BaseSpec.hs
@@ -1,30 +1,30 @@
 module BaseSpec (tests) where
 
-import Data.ByteString.UTF8 qualified as UTF8
-
 import Test.Tasty
 import Test.Tasty.HUnit
+
+import Data.ByteString.UTF8 qualified as UTF8
 
 import Base (matches)
 
 tests :: TestTree
 tests = testGroup "Base"
     [ testGroup "match"
-        [ t True "exact"         "foo"       "fooBar"
-        , t True "caseless"      "FOO"       "fooBar"
-        , t False "invalid"      "["         "any[thing"
-        , t True "alt"           "(foo|az)Q" "bazQux"
-        , t True "dot"           "o.a"       "foobar"
-        , t True "Unicode"       "jör"       "Björk"
-        , t True "dot Unicode"   "j.r"       "Björk"
-        , t True "Nordic case"   "bør"       "BØrnE"
-        , t True "Greek case"    "Λω"        "ΦλΩα"
-        , t True "dot CJK"       "中.人"     "中國人"
+        [ m True "exact"         "foo"       "fooBar"
+        , m True "caseless"      "FOO"       "fooBar"
+        , m False "invalid"      "["         "any[thing"
+        , m True "alt"           "(foo|az)Q" "bazQux"
+        , m True "dot"           "o.a"       "foobar"
+        , m True "Unicode"       "jör"       "Björk"
+        , m True "dot Unicode"   "j.r"       "Björk"
+        , m True "Nordic case"   "bør"       "BØrnE"
+        , m True "Greek case"    "Λω"        "ΦλΩα"
+        , m True "dot CJK"       "中.人"     "中國人"
         ]
     ]
 
 
-t :: Bool -> String -> String -> String -> TestTree
-t b tag pat str =
+m :: Bool -> String -> String -> String -> TestTree
+m b tag pat str =
     testCase tag $ matches (UTF8.fromString pat) (UTF8.fromString str) @?= b
 
