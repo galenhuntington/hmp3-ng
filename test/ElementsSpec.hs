fe357777979fd5b641c3d66417a3edecafd9ec20
Galen Huntington <galen.huntington@gmail.com>
2026-06-27 16:55:53 -0700

    Factor playTitle and add tests.

--- test/ElementsSpec.hs
+++ test/ElementsSpec.hs
@@ -5,7 +5,8 @@ import Test.Tasty.HUnit
 
 import System.Clock (TimeSpec(..))
 
-import Elements (showDuration)
+import Base
+import Elements (showDuration, fitLCR)
 
 tests :: TestTree
 tests = testGroup "Elements"
@@ -35,9 +36,34 @@ tests = testGroup "Elements"
         , testCase "one hour one minute one second"
             $ showDuration True (t 3661)    @?= "1h01m01s"
         ]
+    , testCase "fitLCR" fitTests
     ]
 
 -- Build a TimeSpec from a whole number of seconds.
 t :: Integer -> TimeSpec
 t s = TimeSpec (fromInteger s) 0
+
+fitTests :: Assertion
+fitTests = sequence_ do
+    w <- [1..50]
+    lsz <- [0..20]
+    csz <- [4..8]
+    rsz <- [0..20]
+    let inp = (lsz, csz, rsz)
+    let ans@(lr, padl, padr, ctake) = fitLCR w inp
+    let go as wh = as (wh ++ " " ++ show (w, inp) ++ " -> " ++ show ans)
+    pure do
+        -- Check several invariants.
+        go assertBool "Positive padl" $ padl > 0
+        go assertBool "Positive padr" $ padr > 0 || (ctake == 0 && padr == 0)
+        go assertBool "Nonnegative ctake" $ ctake >= 0
+        go assertEqual "Total width" w $
+            padl + padr + ctake + (if lr then lsz + rsz else 0)
+        go assertEqual "Show iff possible" lr (lsz + csz + rsz + 2 <= w)
+        when (padl > 1 && padr > 1) do
+            go assertEqual "Full center if possible" ctake csz
+            go assertBool "Centered if possible" $
+                let a = padl + (if lr then lsz else 0)
+                    b = padr + (if lr then rsz else 0)
+                in a == b || a + 1 == b
 
